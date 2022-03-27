1d7ec275e5f9dc980c90b212ca8dc2890004609f
emmarp <ruiizemmanuel@gmail.com>
2022-03-26 22:54:14 -0600

    Cambio de colores para la ventana 'GameOver'

--- src/main/java/sample/proyectoloteria/views/GameOver.java
+++ src/main/java/sample/proyectoloteria/views/GameOver.java
@@ -3,6 +3,7 @@ package sample.proyectoloteria.views;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
@@ -33,10 +34,12 @@ public class GameOver extends Stage {
         text1 = new Text();
         text1.setText(message1);
         text1.setFont(Font.font("Malgun Gothic", FontWeight.BOLD, FontPosture.REGULAR, 46));
+        text1.setFill(Color.rgb(254, 249, 239));
 
         text2 = new Text();
         text2.setText(message2);
         text2.setFont(Font.font("Malgun Gothic", FontWeight.BOLD, FontPosture.REGULAR, 24));
+        text2.setFill(Color.rgb(254, 249, 239));
 
         // Layout principal.
         v_box_container = new VBox();
@@ -45,7 +48,7 @@ public class GameOver extends Stage {
         v_box_container.getChildren().addAll(text1, text2);
 
         // Vista principal.
-        scene = new Scene(v_box_container, 400, 250);
+        scene = new Scene(v_box_container, 450, 300);
         // Hoja de estilos CSS.
         scene.getStylesheets().add(String.valueOf(this.getClass().getResource("/style.css")));
     }
