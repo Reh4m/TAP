39da748446c92d9faf5e6ccc7d74e8f1fa52be69
emmarp <ruiizemmanuel@gmail.com>
2022-05-18 22:01:41 -0500

    Carga del archivo 'properties' y asignación del alfabeto Morse

--- src/main/java/sample/proyectoloteria/views/Parser.java
+++ src/main/java/sample/proyectoloteria/views/Parser.java
@@ -18,6 +18,7 @@ import sample.proyectoloteria.util.Images;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class Parser extends Stage implements EventHandler<KeyEvent> {
@@ -30,10 +31,17 @@ public class Parser extends Stage implements EventHandler<KeyEvent> {
     private Image img_open_file;
     private ImageView imv_open_file;
 
-    private final Map<String, String> ALPHABET = new HashMap<>();
+    private final Map<String, String> alphabet = new HashMap<>();
+
+    Properties properties = new Properties();
 
     public Parser() {
+        // Instancia el contenido de la ventana.
         createUI();
+
+        // Carga el alfabeto del Código Morse.
+        loadProperties();
+
         this.setTitle("Traductor de Código Morse");
         this.setScene(scene);
         this.show();
@@ -88,11 +96,8 @@ public class Parser extends Stage implements EventHandler<KeyEvent> {
     }
 
     /**
-     * Obtiene el texto (caracteres) a traducir del archivo especificado.
-     *
-     * El método useDelimiter establece el patrón de delimitación, esto al momento de coincicir con un salto de línea.
-     *
-     * Por último, añadimos los caracteres recuperados al contenido del TextArea.
+     * Recupera el texto (caracteres) a traducir escaneando el contenido del archivo especificado.
+     * Posteriormente, inserta los caracteres recuperados al contenido del TextArea.
      *
      * @param source archivo a escanear.
      * @return cadena de texto a ser traducido.
@@ -101,14 +106,38 @@ public class Parser extends Stage implements EventHandler<KeyEvent> {
         String content = null;
 
         try {
+            // El método useDelimiter establece el patrón de delimitación, esto al momento de coincicir con un salto de
+            // línea al escanear el archivo de texto.
             content = new Scanner(source).useDelimiter("//Z").next();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
+        // Agrega el texto al contenido del TextArea de entrada.
         txt_input.appendText(content);
 
         return content;
+    }
+
+    /**
+     * Lee la lista de propiedades (alfabeto del Código Morse) y lo carga al objeto properties.
+     * Posteriormente, haciendo uso de la interfaz Map, asigna la colección de claves (letras) y valores (código morse)
+     * recuperados desde properties y los asocia a la clase HashMap alphabet.
+     */
+    private void loadProperties() {
+        try {
+            File properties_file = new File("src/main/resources/alphabet.properties");
+
+            // Carga de propiedades.
+            properties.load(new FileReader(properties_file));
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Asignación de claves y valores.
+        for (String letter : properties.stringPropertyNames()) {
+            alphabet.put(letter, properties.getProperty(letter));
+        }
     }
 
     @Override
